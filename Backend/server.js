b809abb3b47e9a77b4acbc462037386eefbe7371
V <v@vbox>
2025-01-18 12:25:47 -0500

    backend_fix

--- Backend/server.js
+++ Backend/server.js
@@ -6,19 +6,18 @@ const dotenv = require('dotenv');
 const cors = require('cors');
 const authRoutes = require('./routes/auth'); // Import the auth routes
 
+const app = express();
 // Use routes
 app.use('/auth', authRoutes); // All authentication routes will start with '/auth'
 
 dotenv.config();
-
-const app = express();
 
 // Middleware
 app.use(express.json());
 app.use(cors());
 
 // MongoDB connection
-mongoose.connect(process.env.MONGO_URI, {
+mongoose.connect('mongodb+srv://Alx:unicloudlib@cluster0.f3j4s.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
